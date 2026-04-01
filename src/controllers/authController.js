2456774ac1de8d0bf8ea10743e7191123fbc47b5
Manas Trivedi <manas.trivedi.020@gmail.com>
2026-04-02 01:10:25 +0530

    Implement authentication with login and registration endpoints, add dotenv for environment variables, and set up Express server

---
+++ src/controllers/authController.js
@@ -0,0 +1,95 @@
+import prisma from "../utils/prisma.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!existingUser) {
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    if (existingUser.status !== "ACTIVE") {
+        return res.status(403).json({ message: "User is inactive" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userid: existingUser.id, role: existingUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const register = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+    if (password.length < 8) {
+      return res.status(400).json({ message: "Password must be at least 8 characters" });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const allowedRoles = ["VIEWER", "ANALYST"];
+
+    if (role && !allowedRoles.includes(role)) {
+        return res.status(400).json({ message: "Invalid role" });
+    }
+
+    const userRole = role || "VIEWER";
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        role: userRole
+      }
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
